dc47930f1ca99f52a0b885464cda1e59c1a0a119
LukeMonty <31657111+LukeMonty@users.noreply.github.com>
2019-03-01 11:46:19 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 
 const app = express();
-let display = {word:"Hello pp"};
+let display = {word:"Hello p"};
 app.get("/api/", (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
