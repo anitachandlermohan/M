99198ab3f3767ccbf9d649b669405fe6530307f5
LukeMonty <31657111+LukeMonty@users.noreply.github.com>
2019-03-01 09:48:24 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
   }
 
   componentDidMount(){
-    let ip = location.host;
     
     fetch("http://51.145.4.86:5000/api").then(res => res.json()).then(jsonString => {
       this.setState({
